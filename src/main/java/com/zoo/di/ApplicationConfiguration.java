7461db79827ffff49fa79b1d9c849c9888ef198a
mszewczyk <szewczykm94@gmail.com>
2018-05-29 13:08:06 +0200

    Added Spring DI configuration, configured Java version to 1.8

---
+++ src/main/java/com/zoo/di/ApplicationConfiguration.java
@@ -0,0 +1,36 @@
+package com.zoo.di;
+
+import com.zoo.ducks.Duck;
+import com.zoo.ducks.SmallDuck;
+import com.zoo.ducks.behaviors.FlyingBehavior;
+import com.zoo.ducks.behaviors.NoFlying;
+import com.zoo.ducks.behaviors.NoQuacking;
+import com.zoo.ducks.behaviors.QuackingBehavior;
+import com.zoo.output.Output;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    Output output() {
+        return System.out::println;
+    }
+
+    @Bean
+    FlyingBehavior flyingBehavior(Output output) {
+        return new NoFlying(output);
+    }
+
+    @Bean
+    QuackingBehavior quackingBehavior(Output output) {
+        return new NoQuacking(output);
+    }
+
+    @Bean
+    Duck duck(FlyingBehavior flyingBehavior, QuackingBehavior quackingBehavior) {
+        return new SmallDuck(flyingBehavior, quackingBehavior);
+    }
+
+}
